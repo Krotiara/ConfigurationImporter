3d7d8d17341ec8466b297faba53be9f518157d21
Krotiara <33466897+Krotiara@users.noreply.github.com>
2023-01-16 20:32:06 +0500

    подпроекты интерфейсов и сущностей, интерфейс и реализация конфигурации

---
+++ ConfigurationImporter.Entities/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using ConfigurationImporter.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationImporter.Entities
+{
+    public class Configuration : IConfiguration
+    {
+        public string Name { get; set ; }
+        public string Description { get; set ; }
+    }
+}
