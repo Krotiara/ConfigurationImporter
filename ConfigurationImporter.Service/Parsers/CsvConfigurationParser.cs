6cc11adda1c5461cafe9e19883ad44c2322573c9
Lisovenko Anton <70dog@mail.ru>
2023-01-17 22:18:10 +0500

    простой csv парсер

--- ConfigurationImporter.Service/Parsers/CsvConfigurationParser.cs
+++ ConfigurationImporter.Service/Parsers/CsvConfigurationParser.cs
@@ -1,4 +1,5 @@
-﻿using ConfigurationImporter.Interfaces;
+﻿using ConfigurationImporter.Entities;
+using ConfigurationImporter.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,18 @@ namespace ConfigurationImporter.Service.Importers
     {
         public IList<IConfiguration> Parse(string filePath)
         {
-            throw new NotImplementedException();
+#warning В рамках задания формат csv соответсвует просто записям без заголовков, где 0 - имя, 1 - описание конфигурации. При расширении форматов csv необходим более умный парсер. В рамках задания сделан простой парсер для определенного формата.
+            using (StreamReader reader = new StreamReader(filePath, Encoding.UTF8))
+            {
+                List<IConfiguration> configurations = new List<IConfiguration>();
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(';');
+                    configurations.Add(new Configuration() { Name = values[0], Description = values[1]});
+                }
+                return configurations;
+            }
         }
     }
 }
