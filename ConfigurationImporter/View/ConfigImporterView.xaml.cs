5d2d8bdd00d2bd8534dd1aa80d7c4996ae896805
Lisovenko Anton <70dog@mail.ru>
2023-01-17 17:12:01 +0500

    Новая стартовая страница, разбиение на MVVM, класс Notifier-а

--- ConfigurationImporter/View/ConfigImporterView.xaml.cs
+++ ConfigurationImporter/View/ConfigImporterView.xaml.cs
@@ -1,4 +1,4 @@
-﻿using ConfigurationImporter.Entities;
+﻿using ConfigurationImporter.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,18 +11,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ConfigurationImporter
+namespace ConfigurationImporter.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для ConfigImporterView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ConfigImporterView : Window
     {
-        public MainWindow()
-        {      
+        public ConfigImporterView(ConfigImporterViewModel viewModel)
+        {
+            DataContext = viewModel;
             InitializeComponent();
         }
     }
---
+++ ConfigurationImporter/ViewModel/ConfigImporterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using NotifierAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConfigurationImporter.Entities.ParserResolverDelegate;
+
+namespace ConfigurationImporter.ViewModel
+{
+    public class ConfigImporterViewModel: Notifier
+    {
+        private readonly ParserResolver parserResolver;
+
+        public ConfigImporterViewModel(ParserResolver parserResolver)
+        {
+            this.parserResolver = parserResolver;
+        }
+
+
+
+    }
+}
