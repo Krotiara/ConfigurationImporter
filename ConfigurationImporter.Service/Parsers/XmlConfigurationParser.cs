95278059d21a6358c41d182af0dd15bb4f8eecab
Lisovenko Anton <70dog@mail.ru>
2023-01-17 22:38:03 +0500

    Update XmlConfigurationParser.cs

--- ConfigurationImporter.Service/Parsers/XmlConfigurationParser.cs
+++ ConfigurationImporter.Service/Parsers/XmlConfigurationParser.cs
@@ -1,10 +1,5 @@
 ﻿using ConfigurationImporter.Entities;
 using ConfigurationImporter.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace ConfigurationImporter.Service.Importers
